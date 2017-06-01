8c534334a54027be4c4087c00d2ee14635612f41
RogerHoney <roger.honeys@gmail.com>
2017-06-01 09:41:35 -0300

    Adicionado classes ConnectionFactory, Contato, ContatoDao, AdicionarContatoSer e o driver para banco de dados

---
+++ src/servlet/AdicionarContatoSer.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.tagext.TryCatchFinally;
+
+import classe.Contato;
+import classe.ContatoDao;
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+
+public class AdicionarContatoSer extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException{
+		//busca o write
+		PrintWriter out = response.getWriter();
+		//buscando os paremetros no request
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		//fazendo conversão da data
+		try {
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+			
+		} catch (Exception e) {
+			out.println("Erro de conversão da data");
+		}
+		
+		//monta um objeto contato
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+		contato.setDataNascimento(dataNascimento);
+		//salva o contato
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+		//imprime o nome do contato que foi adicionado
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato " + contato.getNome() + "foi adicionado com sucesso");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
